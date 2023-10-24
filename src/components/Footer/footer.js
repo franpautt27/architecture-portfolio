b43758f33def052b6b1d1f4c1d6d201bf893e4a6
Francisco Pautt Guzmán <francisco@solving.ai>
2023-10-23 21:52:57 -0500

    design completed

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+    <footer className='footer'>
+        Copyright &#169; Orlando Low. All rights reserved
+    </footer>
+  )
+}
+
+export default Footer
